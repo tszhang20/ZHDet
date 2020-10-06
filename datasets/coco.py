3298a9e4b2f262c6df8053ba5f6bf8670acfb1e2
tszhang20 <1292276333@qq.com>
2020-10-06 20:35:14 +0800

    Initial Commit, datasets

---
+++ datasets/coco.py
@@ -0,0 +1,92 @@
+from torch.utils.data import Dataset
+from pycocotools.coco import COCO
+import numpy as np
+import torch
+import cv2
+import os
+
+COCO_CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+
+COCO_LABEL_MAP = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8,
+                  9: 9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16,
+                  18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24,
+                  27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32,
+                  37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40,
+                  46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48,
+                  54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56,
+                  62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64,
+                  74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72,
+                  82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}
+
+
+class VOCAnnotationTransform:
+    def __init__(self):
+        self.label_map = COCO_LABEL_MAP
+
+    def __call__(self, target, width, height):
+        scale = np.array([width, height, width, height])
+        res = []
+        for obj in target:
+            if "bbox" in obj:
+                bbox = obj["bbox"]
+                # 获得类别id
+                label_idx = self.label_map[obj["category_id"]] - 1
+                # 坐标归一化
+                final_box = list(np.array([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]) / scale)
+                final_box.append(label_idx)
+                res += [final_box]
+            else:
+                print("No bbox found for object", obj)
+        return res
+
+
+class COCODetection(Dataset):
+    def __init__(self, root, ann_path, transform=None, target_transform=VOCAnnotationTransform):
+        self.root = root  # 数据集根目录
+        self.coco = COCO(ann_path)  # COCO类对象
+        self.ids = list(self.coco.imgToAnns.keys())  # 获取所有的图片id号（包含标注信息）
+        self.transform = transform  # 数据增强
+        self.target_transform = target_transform  # 数据变换
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        im, gt, _, _ = self.pull_item(index)  # 返回图像及其对应的标注信息
+        return im, gt
+
+    def pull_item(self, index):
+        img_id = self.ids[index]  # 对应索引的图片id
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)  # 基于图片id获得标注id
+        target = self.coco.loadAnns(ann_ids)  # 加载标注信息
+        # 对应id的图片名称
+        file_name = self.coco.loadImgs(img_id)[0]["file_name"]
+        file_path = os.path.join(self.root, file_name)  # 图片具体路径
+        # 读取图片
+        img = cv2.imread(file_path)
+        height, width, _ = img.shape
+
+        if self.target_transform is not None and len(target):
+            target = self.target_transform()(target, width, height)
+
+        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
+
+
+if __name__ == "__main__":
+    datasets = COCODetection(root="../COCO2017/val2017",
+                             ann_path="../COCO2017/annotations/instances_val2017.json")
+    print(datasets[0])
+    print(len(datasets))
---
+++ datasets/voc.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+import torch
+import numpy as np
+import xml.etree.ElementTree as ET
+from torch.utils.data import Dataset
+
+VOC_CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair',
+               'cow', 'diningtable', 'dog', 'horse',
+               'motorbike', 'person', 'pottedplant',
+               'sheep', 'sofa', 'train', 'tvmonitor')
+
+
+class VOCAnnotationTransform:
+    def __init__(self, class_to_ind=None, keep_difficult=False):
+        # 将符号类别转换为数字类别
+        self.class_to_ind = class_to_ind or dict(
+            zip(VOC_CLASSES, range(len(VOC_CLASSES))))
+        self.keep_difficult = keep_difficult
+
+    def __call__(self, target, width, height):
+        res = []  # 用于存放结果
+        for obj in target.iter('object'):  # 遍历图像中的目标
+            difficult = int(obj.find('difficult').text) == 1
+            if self.keep_difficult and difficult:
+                continue
+            name = obj.find('name').text.lower().strip()  # 类别
+            bbox = obj.find('bndbox')  # 边界框
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']  # 边界框坐标
+            bndbox = []  # 存放边界框的坐标及对应类别
+            for i, pt in enumerate(pts):  # 坐标标准化
+                cur_pt = int(bbox.find(pt).text) - 1
+                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
+                bndbox.append(cur_pt)
+            label_idx = self.class_to_ind[name]
+            bndbox.append(label_idx)  # 添加结果
+            res += [bndbox]
+        return res  # [[xmin,ymin,xmax,ymax,label_ind],...]
+
+
+class VOCDetection(Dataset):
+    def __init__(self, root, image_sets=None, transform=None,
+                 target_transform=VOCAnnotationTransform(),
+                 dataset_name='VOC0712'):
+        if image_sets is None:
+            image_sets = [('2007', 'trainval'), ('2012', 'trainval')]
+        self.root = root  # 数据集根目录
+        self.transform = transform  # 数据变换
+        self.target_transform = target_transform  # 数据增强
+        self.name = dataset_name  # 数据集名称
+
+        self.annopath = os.path.join('%s', 'Annotations', '%s.xml')  # 标注文件路径
+        self.imgpath = os.path.join('%s', 'JPEGImages', '%s.jpg')  # 图像文件路径
+
+        self.ids = list()
+        for (year, name) in image_sets:
+            root_path = os.path.join(self.root, 'VOC' + year)
+            # self.ids=[['/VOC2007','000012'],['/VOC2007','000017'],...]
+            for line in open(os.path.join(root_path, 'ImageSets', 'Main', name + '.txt')):
+                self.ids.append((root_path, line.strip()))
+
+    def __getitem__(self, index):
+        im, gt, _, _ = self.pull_item(index)  # 返回图像及其对应的标注信息
+        return im, gt
+
+    def __len__(self):
+        return len(self.ids)  # 返回数据集数目
+
+    def pull_item(self, index):
+        img_id = self.ids[index]  # 根据index取对应元素
+        target = ET.parse(self.annopath % img_id).getroot()
+        img = cv2.imread(self.imgpath % img_id)
+
+        height, width, _ = img.shape
+
+        if self.target_transform is not None:  # 数据增强
+            target = self.target_transform(target, width, height)
+
+        if self.transform is not None:  # 数据变换
+            target = np.array(target)
+            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            img = img[:, :, (2, 1, 0)]  # 通道变换为RGB
+            target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
+
+    def pull_image(self, index):
+        # 根据索引返回对应图像的OpenCV格式
+        img_id = self.ids[index]
+        return cv2.imread(self.imgpath % img_id, cv2.IMREAD_COLOR)
+
+
+if __name__ == "__main__":
+    datasets = VOCDetection(root="../VOCdevkit")
+    print(datasets[0])
+    print(len(datasets))
